5a740d787dc1b6156350b7d5812d542b227659d3
Mirelle <mirel_miro@hotmail.com>
2025-08-04 22:26:45 +0100

    add prettier

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # File-Upload-Data-Display
+
 Upload a file and display data in that file
+
+# Assumptions
+
+File size isn't specified so we will assume that it can be large data
--- apps/backend/index.ts
+++ apps/backend/index.ts
@@ -1,10 +1,10 @@
-import express from "express";
+import express from 'express';
 
 const app = express();
 const port = 3000;
 
-app.get("/api/hello", (req, res) => {
-  res.json({ message: "Hello from backend!" });
+app.get('/api/hello', (req, res) => {
+  res.json({ message: 'Hello from backend!' });
 });
 
 app.listen(port, () => {
